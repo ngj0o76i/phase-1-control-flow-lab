3f95039620e5620e7eb4e71ebdef06edb81cff20
Nelson <gmz_nlsn@yahoo.com>
2022-08-10 19:12:29 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// function scuberGreetingForFeet(value){
+//   // Write your code here!
+
+// }
+
+// function ternaryCheckCity() {
+//   // Write your code here!
+// }
+
+// function switchOnCharmFromTip() {
+//   // Write your code here!
+// }
+
+const scuberGreetingForFeet = (value) => {
+  let greeting = value;
+  switch (true) {
+    case greeting <= 400:
+
+      return 'This one is on me!';
+
+    case greeting <= 2000:
+
+      return 'That will be twenty bucks.';
+    case greeting >= 2000 && greeting < 2500:
+
+      return 'I will gladly take your thirty bucks.';
+    case greeting > 2500:
+      return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+// console.log(scuberGreetingForFeet(2501));
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+const ternaryCheckCity = (cityName) => cityName === "NYC" ? 'Ok, sounds good.' : 'No go.';
+
+//  console.log (ternaryCheckCity('NYC'));
+
+const switchOnCharmFromTip = (tipAmount) => {
+  if (tipAmount === 'generous') {
+    return 'Thank you so much.';
+  } else if (tipAmount === 'not as generous') {
+    return 'Thank you.';
+  } else {
+    return 'Bye.';
+  }
 }
